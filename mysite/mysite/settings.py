91b4b8da430150f79ac8647cfa4b3b7fc99f127d
Dylandk10 <Dylandk10@gmail.com>
2021-03-29 11:58:49 -0400

    added osrs_api module

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'osrs_api',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
